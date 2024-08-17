87ad0f5448170afd7f28a2339a4c161aad0022a6
Jasimraza Momin <jasimrazamomin@jasimrazas-air.home>
2024-08-17 18:36:24 -0400

    set up tailwind and requirements, started experimenting with next.js and creating pages

---
+++ src/pages/stock_display/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StockDisplayPage = () => {
+  return (
+    <div>
+      <h1>Stock Display</h1>
+      <p>This is where the stock data will be displayed.</p>
+    </div>
+  );
+};
+
+export default StockDisplayPage;
